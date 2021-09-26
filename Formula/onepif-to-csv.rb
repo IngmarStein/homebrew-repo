bffbc1962f601b4a323ad0b42e9e085f91171eda
Ingmar Stein <490610+IngmarStein@users.noreply.github.com>
2021-09-26 19:17:53 +0000

    onepif-to-csv: add 0.3 bottle.

--- Formula/onepif-to-csv.rb
+++ Formula/onepif-to-csv.rb
@@ -5,6 +5,13 @@ class OnepifToCsv < Formula
   sha256 "c4cad55818ab83f5a04bcd26c9c75ebed76acf69efb2e5400e4d051020163290"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/IngmarStein/homebrew-repo/releases/download/onepif-to-csv-0.3"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, catalina:     "bad3b910d7dc516f109d6419c21551056cff1c3ea626505359c38593d83053e6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "16f720d5bd8dca19db1411b127d4a185b4e2115e2756c7ed0b6b588c00c6a9fb"
+  end
+
   depends_on "go" => :build
 
   def install
