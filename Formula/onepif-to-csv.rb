161ff24cd11bd46b9b6a3deb52faaa44b716714a
Ingmar Stein <IngmarStein@users.noreply.github.com>
2021-09-26 20:28:22 +0200

    Add 1pif-to-csv formula

---
+++ Formula/onepif-to-csv.rb
@@ -0,0 +1,17 @@
+class OnepifToCsv < Formula
+  desc "Convert 1pif files from 1Password to CSV"
+  homepage "https://github.com/IngmarStein"
+  url "https://github.com/IngmarStein/1pif-to-csv/archive/refs/tags/v0.3.tar.gz"
+  sha256 "c4cad55818ab83f5a04bcd26c9c75ebed76acf69efb2e5400e4d051020163290"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", bin/"1pif-to-csv"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/1pif-to-csv --version")
+  end
+end
